8f8272d0703b158582054307900dfa5cb61aeea7
Mounir IDRASSI <mounir.idrassi@idrix.fr>
2013-06-22 17:39:39 +0200

    Add bat file for signing VeraCrypt drivers and binaries, and all generating a signed installer

---
+++ src/Signing/sign.bat
@@ -0,0 +1,17 @@
+PATH=%PATH%;%DDK%\bin\x86
+
+signtool sign /v /a /ac thawte_Primary_MS_Cross_Cert.cer /t http://timestamp.verisign.com/scripts/timestamp.dll "..\Release\Setup Files\veracrypt.sys"
+signtool sign /v /a /ac thawte_Primary_MS_Cross_Cert.cer /t http://timestamp.verisign.com/scripts/timestamp.dll "..\Release\Setup Files\veracrypt-x64.sys"
+
+signtool sign /v /a /ac Thawt_CodeSigning_CA.crt /t http://timestamp.verisign.com/scripts/timestamp.dll "..\Release\Setup Files\VeraCrypt.exe"
+signtool sign /v /a /ac Thawt_CodeSigning_CA.crt /t http://timestamp.verisign.com/scripts/timestamp.dll "..\Release\Setup Files\VeraCrypt Format.exe"
+
+cd "..\Release\Setup Files\"
+
+"VeraCrypt Setup.exe" /p
+
+cd "..\..\Signing"
+
+signtool sign /v /a /ac Thawt_CodeSigning_CA.crt /t http://timestamp.verisign.com/scripts/timestamp.dll "..\Release\Setup Files\VeraCrypt Setup 7.1a.exe"
+
+pause
