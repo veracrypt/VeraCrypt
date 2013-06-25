a8fecdcbb71f3598232a4eab175c4cf91de7b5f8
Mounir IDRASSI <mounir.idrassi@idrix.fr>
2013-06-25 05:50:20 +0200

    restore TrueCrypt reference in license header

--- src/Setup/Setup.c
+++ src/Setup/Setup.c
@@ -4,7 +4,7 @@
  Copyright (c) 1998-2000 Paul Le Roux and which is governed by the 'License
  Agreement for Encryption for the Masses'. Modifications and additions to
  the original source code (contained in this file) and all other portions
- of this file are Copyright (c) 2003-2012 VeraCrypt Developers Association
+ of this file are Copyright (c) 2003-2012 TrueCrypt Developers Association
  and are governed by the TrueCrypt License 3.0 the full text of which is
  contained in the file License.txt included in VeraCrypt binary and source
  code distribution packages. */
