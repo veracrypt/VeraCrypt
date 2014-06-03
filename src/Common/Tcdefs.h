08e653c9261b7291cfc31738c8f400e70d29432a
Mounir IDRASSI <mounir.idrassi@idrix.fr>
2014-06-03 09:14:04 +0200

    Increment VeraCrypt version to 1.0d

--- src/Common/Tcdefs.h
+++ src/Common/Tcdefs.h
@@ -15,15 +15,15 @@
 #define TC_APP_NAME						"VeraCrypt"
 
 // Version displayed to user 
-#define VERSION_STRING					"1.0c"
+#define VERSION_STRING					"1.0d"
 
 // Version number to compare against driver
-#define VERSION_NUM						0x010c
+#define VERSION_NUM						0x010d
 
 // Release date
-#define TC_STR_RELEASE_DATE				"November 5, 2013"
-#define TC_RELEASE_DATE_YEAR			2013
-#define TC_RELEASE_DATE_MONTH			11
+#define TC_STR_RELEASE_DATE				"June 3, 2014"
+#define TC_RELEASE_DATE_YEAR			2014
+#define TC_RELEASE_DATE_MONTH			06
 
 #define BYTES_PER_KB                    1024LL
 #define BYTES_PER_MB                    1048576LL
