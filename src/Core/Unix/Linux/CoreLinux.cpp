facfda73eb607b8aba0c90f54d82c19ba057b0c9
Mounir IDRASSI <mounir.idrassi@idrix.fr>
2016-02-21 23:57:12 +0100

    Linux: fix mount issue on Fedora 23 by forcing the creation of a default loop device.

--- src/Core/Unix/Linux/CoreLinux.cpp
+++ src/Core/Unix/Linux/CoreLinux.cpp
@@ -41,6 +41,17 @@ namespace VeraCrypt
 		loopPaths.push_back ("/dev/loop");
 		loopPaths.push_back ("/dev/loop/");
 		loopPaths.push_back ("/dev/.static/dev/loop");
+		
+		// On Fedora 23,"losetup -f" must be called first to create a default loop device
+		list <string> args;
+		args.push_back ("-f");
+
+		try
+		{
+			Process::Execute ("losetup", args);
+			break;
+		}
+		catch (...) { }
 
 		for (int devIndex = 0; devIndex < 256; devIndex++)
 		{
