ba1fbb688edf7db0edf3ea9d24a95d5a5ef2260c
Mounir IDRASSI <mounir.idrassi@idrix.fr>
2017-06-13 01:23:54 +0200

    Linux: fix executable stack in resulting binary which was caused by crypto assembly files missing the GNU-stack note.

--- src/Crypto/Aes_hw_cpu.asm
+++ src/Crypto/Aes_hw_cpu.asm
@@ -331,3 +331,14 @@
 
 
 %endif	; __BITS__ != 16
+
+%ifidn __OUTPUT_FORMAT__,elf
+section .note.GNU-stack noalloc noexec nowrite progbits
+%endif
+%ifidn __OUTPUT_FORMAT__,elf32
+section .note.GNU-stack noalloc noexec nowrite progbits
+%endif
+%ifidn __OUTPUT_FORMAT__,elf64
+section .note.GNU-stack noalloc noexec nowrite progbits
+%endif
+
