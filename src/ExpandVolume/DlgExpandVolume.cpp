0ebc26e12541e585ea42ce3d1e0faa04f7720efa
Mounir IDRASSI <mounir.idrassi@idrix.fr>
2017-06-12 11:16:25 +0200

    Update IDRIX copyright year

--- src/ExpandVolume/DlgExpandVolume.cpp
+++ src/ExpandVolume/DlgExpandVolume.cpp
@@ -9,7 +9,7 @@
  or Copyright (c) 2012-2013 Josef Schneider <josef@netpage.dk>
 
  Modifications and additions to the original source code (contained in this file)
- and all other portions of this file are Copyright (c) 2013-2016 IDRIX
+ and all other portions of this file are Copyright (c) 2013-2017 IDRIX
  and are governed by the Apache License 2.0 the full text of which is
  contained in the file License.txt included in VeraCrypt binary and source
  code distribution packages. */
