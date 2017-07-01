3c4e36ecdf24efc89465a11fc7803b235c88881d
Mounir IDRASSI <mounir.idrassi@idrix.fr>
2017-07-01 11:08:29 +0200

    Increment version to 1.21 (1.21.1)

--- src/Common/Tcdefs.h
+++ src/Common/Tcdefs.h
@@ -55,15 +55,15 @@ extern unsigned short _rotl16(unsigned short value, unsigned char shift);
 #define TC_APP_NAME						"VeraCrypt"
 
 // Version displayed to user 
-#define VERSION_STRING					"1.20"
+#define VERSION_STRING					"1.21"
 
 // Version number to compare against driver
-#define VERSION_NUM						0x0120
+#define VERSION_NUM						0x0121
 
 // Release date
-#define TC_STR_RELEASE_DATE			L"June 29, 2017"
+#define TC_STR_RELEASE_DATE			L"July 1, 2017"
 #define TC_RELEASE_DATE_YEAR			2017
-#define TC_RELEASE_DATE_MONTH			 06
+#define TC_RELEASE_DATE_MONTH			 07
 
 #define BYTES_PER_KB                    1024LL
 #define BYTES_PER_MB                    1048576LL
