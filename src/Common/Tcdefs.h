2ef3c3a4821fbbe65f4d144df2984800d69ad121
Mounir IDRASSI <mounir.idrassi@idrix.fr>
2017-12-09 12:44:27 +0100

    Increment version to 1.22-BETA4 (1.22.4)

--- src/Common/Tcdefs.h
+++ src/Common/Tcdefs.h
@@ -55,15 +55,15 @@ extern unsigned short _rotl16(unsigned short value, unsigned char shift);
 #define TC_APP_NAME						"VeraCrypt"
 
 // Version displayed to user 
-#define VERSION_STRING					"1.22-BETA3"
+#define VERSION_STRING					"1.22-BETA4"
 
 // Version number to compare against driver
 #define VERSION_NUM						0x0122
 
 // Release date
-#define TC_STR_RELEASE_DATE			L"August 2, 2017"
+#define TC_STR_RELEASE_DATE			L"December 8, 2017"
 #define TC_RELEASE_DATE_YEAR			2017
-#define TC_RELEASE_DATE_MONTH			 08
+#define TC_RELEASE_DATE_MONTH			 12
 
 #define BYTES_PER_KB                    1024LL
 #define BYTES_PER_MB                    1048576LL
