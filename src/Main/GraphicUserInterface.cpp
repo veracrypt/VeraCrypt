29b80ec6df43a48877efdeba68d59d78ee4e8074
Mounir IDRASSI <mounir.idrassi@idrix.fr>
2018-04-05 23:39:48 +0200

    FreeBSD/Linux/OSX: replace some remaining TrueCrypt.tc file extension in the code by VeraCrypt .hc extension

--- src/Main/GraphicUserInterface.cpp
+++ src/Main/GraphicUserInterface.cpp
@@ -1650,7 +1650,7 @@ namespace VeraCrypt
 	FilePath GraphicUserInterface::SelectVolumeFile (wxWindow *parent, bool saveMode, const DirectoryPath &directory) const
 	{
 		list < pair <wstring, wstring> > extensions;
-		extensions.push_back (make_pair (L"tc", LangString["TC_VOLUMES"].ToStdWstring()));
+		extensions.push_back (make_pair (L"hc", LangString["TC_VOLUMES"].ToStdWstring()));
 
 		FilePathList selFiles = Gui->SelectFiles (parent, LangString[saveMode ? "OPEN_NEW_VOLUME" : "OPEN_VOL_TITLE"], saveMode, false, extensions, directory);
 
