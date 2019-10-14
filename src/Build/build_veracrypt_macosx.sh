67a514da6f3552fc075d9ae54557de4a752a7e47
El Mostafa Idrassi <el-mostafa.idrassi@prestalab.net>
2019-10-14 23:03:36 +0100

    MacOSX: Enhanced Makefile and build script (#519)
    Added new variable 'VC_OSX_SDK' to differentiate between the OSX minimum target and the SDK to use for building.

--- src/Build/build_veracrypt_macosx.sh
+++ src/Build/build_veracrypt_macosx.sh
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2013-2017 IDRIX
+# Copyright (c) 2013-2019 IDRIX
 # Governed by the Apache License 2.0 the full text of which is contained
 # in the file License.txt included in VeraCrypt binary and source
 # code distribution packages.
@@ -12,22 +12,22 @@ SOURCEPATH=$(cd "$(dirname "$SCRIPTPATH/../.")"; pwd)
 # directory where the VeraCrypt project has been checked out
 PARENTDIR=$(cd "$(dirname "$SCRIPTPATH/../../../.")"; pwd)
 
-# the sources of wxWidgets 3.1.2 must be extracted to the parent directory
+# the sources of wxWidgets 3.1.2 must be extracted to the parent directory (for night mode)
 export WX_ROOT=$PARENTDIR/wxWidgets-3.1.2
 echo "Using wxWidgets sources in $WX_ROOT"
 
 # this will be the temporary wxWidgets directory
 export WX_BUILD_DIR=$PARENTDIR/wxBuild-3.1.2
 
-# define the SDK version to use. We use 10.9 by default
+# define the SDK version to use and OSX minimum target. We target 10.9 by default
 export VC_OSX_TARGET=10.9
-echo "Using MacOSX SDK $VC_OSX_TARGET"
-
+export VC_OSX_SDK=10.14
+echo "Using MacOSX SDK $VC_OSX_SDK"
 
 cd $SOURCEPATH
 
 echo "Building VeraCrypt"
-# make WXSTATIC=FULL wxbuild && make WXSTATIC=FULL clean && make WXSTATIC=FULL && make WXSTATIC=FULL package
+make WXSTATIC=FULL wxbuild && make WXSTATIC=FULL clean && make WXSTATIC=FULL && make WXSTATIC=FULL package
 
 # Uncomment below and comment line above to reuse existing wxWidgets build
-make WXSTATIC=FULL clean && make WXSTATIC=FULL && make WXSTATIC=FULL package
+# make WXSTATIC=FULL clean && make WXSTATIC=FULL && make WXSTATIC=FULL package
