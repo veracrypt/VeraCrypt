6bf37280d621d3c8fae85cf4e0533df0bc8334ca
Mounir IDRASSI <mounir.idrassi@idrix.fr>
2019-10-25 22:46:17 +0200

    MacOSX: use wxWidgets 3.1.2 for the legacy build which is now done with SDK 10.9 while still targeting OSX 10.7.

--- src/Build/build_veracrypt_macosx_legacy.sh
+++ src/Build/build_veracrypt_macosx_legacy.sh
@@ -12,21 +12,22 @@ SOURCEPATH=$(cd "$(dirname "$SCRIPTPATH/../.")"; pwd)
 # directory where the VeraCrypt project has been checked out
 PARENTDIR=$(cd "$(dirname "$SCRIPTPATH/../../../.")"; pwd)
 
-# the sources of wxWidgets 3.0.4 must be extracted to the parent directory
-export WX_ROOT=$PARENTDIR/wxWidgets-3.0.4
+# the sources of wxWidgets 3.1.2 must be extracted to the parent directory
+export WX_ROOT=$PARENTDIR/wxWidgets-3.1.2
 echo "Using wxWidgets sources in $WX_ROOT"
 # this will be the temporary wxWidgets directory
 export WX_BUILD_DIR=$PARENTDIR/wxBuild
 
 # define the SDK version to use. We use 10.7 by default
 export VC_OSX_TARGET=10.7
+export VC_OSX_SDK=10.9
 echo "Using MacOSX SDK $VC_OSX_TARGET"
 
 
 cd $SOURCEPATH
 
 echo "Building VeraCrypt"
-make WXSTATIC=1 wxbuild && make WXSTATIC=1 clean && make WXSTATIC=1 && make WXSTATIC=1 package
+make WXSTATIC=FULL wxbuild && make WXSTATIC=FULL clean && make WXSTATIC=FULL && make WXSTATIC=FULL package
 
 # Uncomment below and comment line above to reuse existing wxWidgets build
-# make WXSTATIC=1 clean && make WXSTATIC=1 && make WXSTATIC=1 package
+# make WXSTATIC=FULL clean && make WXSTATIC=FULL && make WXSTATIC=FULL package
