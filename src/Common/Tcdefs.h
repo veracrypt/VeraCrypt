cb1efdc9ae47d13d2c1a10bf7ec3a5308ffbfa32
Mounir IDRASSI <mounir.idrassi@idrix.fr>
2020-06-23 15:06:02 +0200

    Increment version to 1.24-Update7

--- src/Common/Tcdefs.h
+++ src/Common/Tcdefs.h
@@ -55,7 +55,7 @@ extern unsigned short _rotl16(unsigned short value, unsigned char shift);
 #define TC_APP_NAME						"VeraCrypt"
 
 // Version displayed to user 
-#define VERSION_STRING					"1.24-Update6"
+#define VERSION_STRING					"1.24-Update7"
 
 #ifdef VC_EFI_CUSTOM_MODE
 #define VERSION_STRING_SUFFIX			"-CustomEFI"
@@ -67,9 +67,9 @@ extern unsigned short _rotl16(unsigned short value, unsigned char shift);
 #define VERSION_NUM						0x0124
 
 // Release date
-#define TC_STR_RELEASE_DATE			L"March 10, 2020"
+#define TC_STR_RELEASE_DATE			L"June 23, 2020"
 #define TC_RELEASE_DATE_YEAR			2020
-#define TC_RELEASE_DATE_MONTH			 3
+#define TC_RELEASE_DATE_MONTH			 6
 
 #define BYTES_PER_KB                    1024LL
 #define BYTES_PER_MB                    1048576LL
