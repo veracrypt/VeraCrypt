849cd010e8b30060893f0fa39ed2d7742aba6d11
Unit 193 <32967979+unit193@users.noreply.github.com>
2021-08-15 18:54:21 -0400

    Add compile-time toggleable support for application indicators, off by default. (#815)
    This tries to figure out, via wx-config, if you're using GTK2 or GTK3
    and uses the associated Ayatana library.

--- src/Main/GraphicUserInterface.cpp
+++ src/Main/GraphicUserInterface.cpp
@@ -1754,6 +1754,10 @@ namespace VeraCrypt
 		}
 
 		BackgroundMode = state;
+
+#ifdef HAVE_INDICATORS
+		gtk_menu_item_set_label ((GtkMenuItem*) ((MainFrame*) mMainFrame)->indicator_item_showhide, LangString[Gui->IsInBackgroundMode() ? "SHOW_TC" : "HIDE_TC"].mb_str());
+#endif
 	}
 
 	void GraphicUserInterface::SetListCtrlColumnWidths (wxListCtrl *listCtrl, list <int> columnWidthPermilles, bool hasVerticalScrollbar) const
