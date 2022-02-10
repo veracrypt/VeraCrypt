b5b252107df9134a03da42302f3fb22693809680
Mounir IDRASSI <mounir.idrassi@idrix.fr>
2022-02-10 23:28:02 +0100

    Update Release Notes and set release date to February 10th.

--- src/Common/Tcdefs.h
+++ src/Common/Tcdefs.h
@@ -69,7 +69,7 @@ extern unsigned short _rotl16(unsigned short value, unsigned char shift);
 #define VERSION_NUM						0x0125
 
 // Release date
-#define TC_STR_RELEASE_DATE			L"February 5, 2022"
+#define TC_STR_RELEASE_DATE			L"February 10, 2022"
 #define TC_RELEASE_DATE_YEAR			2022
 #define TC_RELEASE_DATE_MONTH			 02
 
