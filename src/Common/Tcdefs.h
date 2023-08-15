e47f6e900cb129b6dfee6457785a6b7fe2633c62
Mounir IDRASSI <mounir.idrassi@idrix.fr>
2023-08-15 09:48:51 +0200

    Update Release Notes and set date of 1.26.5 to August 15th

--- src/Common/Tcdefs.h
+++ src/Common/Tcdefs.h
@@ -73,7 +73,7 @@ extern unsigned short _rotl16(unsigned short value, unsigned char shift);
 #define VERSION_NUM						0x0126
 
 // Release date
-#define TC_STR_RELEASE_DATE			L"August 14, 2023"
+#define TC_STR_RELEASE_DATE			L"August 15, 2023"
 #define TC_RELEASE_DATE_YEAR			2023
 #define TC_RELEASE_DATE_MONTH			 07
 
