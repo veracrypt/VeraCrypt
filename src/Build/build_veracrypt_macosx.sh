ad431d95a8e9229adf62ade5e1bfcfbac5822127
Deniz Tuerkoglu <turkoglu.deniz@gmail.com>
2023-08-20 20:12:58 +1000

    Add flag to use homebrew packages
    When building, we can use prebuilt wxwidgets from homebrew to
    simplify and speed up local building. We also put the package
    behind a flag as it's optional during development.

--- src/Build/build_veracrypt_macosx.sh
+++ src/Build/build_veracrypt_macosx.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 #
 # Copyright (c) 2013-2019 IDRIX
 # Governed by the Apache License 2.0 the full text of which is contained
@@ -11,6 +13,32 @@ SCRIPTPATH=$(cd "$(dirname "$0")"; pwd)
 SOURCEPATH=$(cd "$(dirname "$SCRIPTPATH/../.")"; pwd)
 # directory where the VeraCrypt project has been checked out
 PARENTDIR=$(cd "$(dirname "$SCRIPTPATH/../../../.")"; pwd)
+
+while getopts bpr flag
+do
+    case "${flag}" in
+        b) brew=true;;
+        p) package=true;;
+    esac
+done
+
+if [ -n "$brew" ]; then
+    export VC_OSX_TARGET=10.9
+    export VC_OSX_SDK=$(xcrun --show-sdk-version) #use the latest version installed, this might fail
+    echo "Using MacOSX SDK $VC_OSX_SDK with target set to $VC_OSX_TARGET"
+    cd $SOURCEPATH
+
+    echo "Building VeraCrypt with precompiled homebrew packages"
+    cellar=$(brew --cellar "wxwidgets")
+    version=$(brew list --versions "wxwidgets" | head -1 | awk '{print $2}')
+    export WX_BUILD_DIR="$cellar/$version/bin"
+
+    make clean && make
+    if [ -n "$package" ]; then
+        make package
+    fi
+    exit 0
+fi
 
 # the sources of wxWidgets 3.1.2 must be extracted to the parent directory (for night mode)
 export WX_ROOT=$PARENTDIR/wxWidgets-3.2.2.1
