d02734dfaa85c583947358cf1ec1d4b426fb67a0
Mounir IDRASSI <mounir.idrassi@idrix.fr>
2023-09-21 01:31:35 +0200

    Update Release Notes. Increment version to 1.26.6.

--- src/Common/Tcdefs.h
+++ src/Common/Tcdefs.h
@@ -59,7 +59,7 @@ extern unsigned short _rotl16(unsigned short value, unsigned char shift);
 #define TC_APP_NAME						"VeraCrypt"
 
 // Version displayed to user 
-#define VERSION_STRING					"1.26.5"
+#define VERSION_STRING					"1.26.6"
 
 #ifdef VC_EFI_CUSTOM_MODE
 #define VERSION_STRING_SUFFIX			"-CustomEFI"
@@ -73,9 +73,9 @@ extern unsigned short _rotl16(unsigned short value, unsigned char shift);
 #define VERSION_NUM						0x0126
 
 // Release date
-#define TC_STR_RELEASE_DATE			L"August 15, 2023"
+#define TC_STR_RELEASE_DATE			L"September 21, 2023"
 #define TC_RELEASE_DATE_YEAR			2023
-#define TC_RELEASE_DATE_MONTH			 07
+#define TC_RELEASE_DATE_MONTH			 09
 
 #define BYTES_PER_KB                    1024LL
 #define BYTES_PER_MB                    1048576LL
