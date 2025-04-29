97e01098d4a0c1899e46b22e1a5b8e7ebe5a7e86
Mounir IDRASSI <mounir.idrassi@idrix.fr>
2025-04-29 11:45:12 +0900

    Documentation: various fixes. Add Russian CHM file.

--- doc/chm/create_chm.bat
+++ doc/chm/create_chm.bat
@@ -23,6 +23,17 @@ rmdir /s /Q CompilingGuidelineWin
 
 move /Y "VeraCrypt User Guide.zh-cn.chm" "..\VeraCrypt User Guide.zh-cn.chm"
 
+cd %CHMBUILDPATH%\ru
+
+xcopy /E ..\..\html\ru\* .
+
+hhc VeraCrypt_ru.hhp
+
+del /F /Q *.html *.css *.jpg *.gif *.png *.svg *.js
+rmdir /s /Q CompilingGuidelineWin
+
+move /Y "VeraCrypt User Guide.ru.chm" "..\VeraCrypt User Guide.ru.chm"
+
 cd %CHMBUILDPATH%
 
 
