d0822a535b52e429eb8d7a83036b34cbea16fd34
Jertzukka <jertzukka@gmail.com>
2025-05-26 07:24:41 +0300

    All platforms: Update logo icons Update the old logo with a simplified one without extra label text. .ico and .icns are updated on Windows and macOS, and .png, .svg are added on Linux and FreeBSD.
    Original logo design by Andreas Becker, recreated by danielwerg with
    some edits and symbolic version by me.

--- src/Setup/FreeBSD/veracrypt-uninstall.sh
+++ src/Setup/FreeBSD/veracrypt-uninstall.sh
@@ -7,13 +7,19 @@ removal_failed() {
 }
 
 rm -f /usr/bin/veracrypt || removal_failed
-rm -f /usr/share/applications/veracrypt.desktop || removal_failed
-rm -f /usr/share/pixmaps/veracrypt.xpm || removal_failed
-rm -f /usr/share/mime/packages/veracrypt.xml || removal_failed
+rm -f /usr/local/share/applications/veracrypt.desktop || removal_failed
+rm -f /usr/local/share/pixmaps/veracrypt.xpm || removal_failed
+rm -f /usr/local/share/mime/packages/veracrypt.xml || removal_failed
 rm -fr /usr/share/veracrypt || removal_failed
 rm -fr /usr/share/doc/veracrypt || removal_failed
+rm -f /usr/local/share/icons/hicolor/scalable/apps/veracrypt.svg || removal_failed
+rm -f /usr/local/share/icons/hicolor/symbolic/apps/veracrypt-symbolic.svg || removal_failed
+for res in 16 22 24 32 48 64 256 512 1024; do \
+		rm -f /usr/share/icons/hicolor/${res}x${res}/apps/veracrypt.png || removal_failed ;\
+done
+
 rm -f /usr/bin/veracrypt-uninstall.sh || removal_failed
-update-mime-database /usr/share/mime >/dev/null 2>&1
+update-mime-database /usr/local/share/mime >/dev/null 2>&1
 update-desktop-database -q
 
 echo VeraCrypt uninstalled.
