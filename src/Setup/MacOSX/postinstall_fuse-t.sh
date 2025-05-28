b64b7c9ca42b5efeb5096364d23ad5a03a313478
Mounir IDRASSI <mounir.idrassi@amcrypto.jp>
2025-05-29 05:48:40 +0900

    macOS: Create symlink to VeraCrypt binary at /usr/local/bin/veracrypt during installation
    This makes it easier to use the VeraCrypt CLI in a manner similar to Linux systems.

--- src/Setup/MacOSX/postinstall_fuse-t.sh
+++ src/Setup/MacOSX/postinstall_fuse-t.sh
@@ -2,4 +2,10 @@
 
 chmod -R go-w /Applications/VeraCrypt.app
 
+# create simlink to VeraCrypt binary in /usr/local/bin
+if !([ -e "/usr/local/bin/veracrypt" ])
+then
+    ln -s /Applications/VeraCrypt.app/Contents/MacOS/VeraCrypt /usr/local/bin/veracrypt
+fi
+
 exit 0
