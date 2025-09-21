b1007c61353946ce1343dadc9f38cef05f04425f
Mounir IDRASSI <mounir.idrassi@amcrypto.jp>
2025-09-21 11:56:50 +0900

    Increment version to 1.26.27. Update Release notes, Windows driver, bootloader

--- src/Common/Tcdefs.h
+++ src/Common/Tcdefs.h
@@ -59,7 +59,7 @@ extern unsigned short _rotl16(unsigned short value, unsigned char shift);
 #define TC_APP_NAME						"VeraCrypt"
 
 // Version displayed to user 
-#define VERSION_STRING					"1.26.26"
+#define VERSION_STRING					"1.26.27"
 
 #ifdef VC_EFI_CUSTOM_MODE
 #define VERSION_STRING_SUFFIX			"-CustomEFI"
@@ -73,9 +73,9 @@ extern unsigned short _rotl16(unsigned short value, unsigned char shift);
 #define VERSION_NUM						0x0126
 
 // Release date
-#define TC_STR_RELEASE_DATE			L"June 29, 2025"
+#define TC_STR_RELEASE_DATE			L"September 20, 2025"
 #define TC_RELEASE_DATE_YEAR			2025
-#define TC_RELEASE_DATE_MONTH			 6
+#define TC_RELEASE_DATE_MONTH			 9
 
 #define BYTES_PER_KB                    1024LL
 #define BYTES_PER_MB                    1048576LL
